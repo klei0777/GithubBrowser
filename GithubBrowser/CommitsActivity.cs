d5db8a95d25ebe19fb77579b5de5d84e4d8cae06
Chris Kleinhans <Christopher.Kleinhans@snapon.com>
2021-09-11 17:45:26 -0400

    Added a ListView to the commits layout. Updated the CommitsActivity to populate the ListView with the Message of each commit.

--- GithubBrowser/CommitsActivity.cs
+++ GithubBrowser/CommitsActivity.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 using GithubBrowser.Model;
 using Newtonsoft.Json;
@@ -17,6 +19,48 @@ namespace GithubBrowser
             SetContentView(Resource.Layout.commits);
 
             List<CommitModel> commits = JsonConvert.DeserializeObject<List<CommitModel>>(Intent.GetStringExtra("commits"));
+
+            var listView = FindViewById<ListView>(Resource.Id.listViewCommits);
+            listView.Adapter = new CommitModelAdapter(this, commits);
+        }
+
+        private class CommitModelAdapter : BaseAdapter<CommitModel>
+        {
+            List<CommitModel> items;
+            Android.App.Activity context;
+
+            public CommitModelAdapter(Android.App.Activity context, List<CommitModel> items)
+                : base()
+            {
+                this.context = context;
+                this.items = items;
+            }
+
+            public override long GetItemId(int position)
+            {
+                return position;
+            }
+
+            public override CommitModel this[int position]
+            {
+                get { return items[position]; }
+            }
+
+            public override int Count
+            {
+                get { return items.Count; }
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                View view = convertView;
+                if (view == null)
+                {
+                    view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+                }
+                view.FindViewById<TextView>(Android.Resource.Id.Text1).Text = items[position].Message;
+                return view;
+            }
         }
     }
 }
