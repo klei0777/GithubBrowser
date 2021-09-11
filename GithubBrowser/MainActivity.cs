40ccacfae82f5b0b0bc9637bac165ff7f636867c
Chris Kleinhans <Christopher.Kleinhans@snapon.com>
2021-09-11 14:02:48 -0400

    Add project files.

---
+++ GithubBrowser/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+
+namespace GithubBrowser
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+	}
+}
