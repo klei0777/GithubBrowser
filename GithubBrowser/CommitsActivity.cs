973093109b3fe31d767afa17956e4c768130d932
Chris Kleinhans <Christopher.Kleinhans@snapon.com>
2021-09-11 16:58:46 -0400

    Added a layout and activity for displaying commits.

---
+++ GithubBrowser/CommitsActivity.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace GithubBrowser
+{
+    [Activity(Label = "CommitsActivity")]
+    public class CommitsActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.commits);
+        }
+    }
+}
